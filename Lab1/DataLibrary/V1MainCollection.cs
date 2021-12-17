56daf62204eb3b5ced606ebe83d52b51c8d18675
AlbertIvm <smyaosan@gmail.com>
2021-12-17 11:07:00 +0300

    Implement V1MainCollection, add newlines at the end of files

--- Lab1/DataLibrary/V1MainCollection.cs
+++ Lab1/DataLibrary/V1MainCollection.cs
@@ -1,7 +1,59 @@
-﻿namespace DataLibrary
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary
 {
     public class V1MainCollection
     {
+        private List<V1Data> Data;
 
+        public int Count { get => Data.Count; }
+
+        public V1Data this[int index] { get => Data[index]; }
+
+        public bool Contains(string id)
+        {
+            foreach (var item in Data)
+            {
+                if (item.ID == id)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Add(V1Data v1Data)
+        {
+            if (Contains(v1Data.ID))
+            {
+                return false;
+            }
+            else
+            {
+                Data.Add(v1Data);
+                return true;
+            }
+        }
+
+        public string ToLongString(string format)
+        {
+            StringBuilder builder = new("V1MainCollection with items:\n");
+            foreach (var item in Data)
+            {
+                builder.AppendLine(item.ToLongString(format));
+            }
+            return builder.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new("V1MainCollection with items:\n");
+            foreach (var item in Data)
+            {
+                builder.AppendLine(item.ToString());
+            }
+            return builder.ToString();
+        }
     }
 }
