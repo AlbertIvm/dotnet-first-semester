4d6478021748b2f17b997b19706cbb87b9d1e78e
AlbertIvm <smyaosan@gmail.com>
2021-12-17 11:24:13 +0300

    Add static class for field calculation

--- Lab1/DataLibrary/Field.cs
+++ Lab1/DataLibrary/Field.cs
@@ -1,7 +1,23 @@
-﻿namespace DataLibrary
+﻿using System;
+using System.Numerics;
+
+namespace DataLibrary
 {
     public static class Field
     {
+        public static Complex UniformField(double x, double y)
+        {
+            return new Complex(3.0, 4.0);
+        }
 
+        public static Complex FieldFacingInfinity(double x, double y)
+        {
+            double radius = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            if (radius < 1e-6)
+            {
+                return Complex.Zero; // For safety
+            }
+            return new Complex(x / Math.Pow(radius, 3), y / Math.Pow(radius, 3));
+        }
     }
 }
