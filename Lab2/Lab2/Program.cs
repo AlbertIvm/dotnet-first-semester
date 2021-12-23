0a8d4150da41871df98c0cbf307a6d1f5245c65e
AlbertIvm <smyaosan@gmail.com>
2021-12-23 03:44:11 +0300

    Add test for LINQ properties

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using DataLibrary;
 
 namespace Lab2
@@ -63,7 +64,59 @@ namespace Lab2
 
         static void TestLINQ()
         {
+            V1MainCollection collection = new();
+            Console.WriteLine("---------- Method calls for empty collection: ----------");
+            Console.WriteLine($"AverageMagnitude: {collection.AverageMagnitude}");
+            Console.WriteLine($"MostDeviated: {collection.MostDeviated}");
+            Console.WriteLine($"CommonXCoords:");
+            var iterator = collection.CommonXCoords;
+            if (iterator != null)
+            {
+                foreach (var item in iterator)
+                {
+                    Console.WriteLine($"  {item}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"no elements, got null");
+            }
 
+            collection.Add(new V1DataArray("Empty array", DateTime.Now, 0, 0, 0, 0, Field.UniformField));
+            collection.Add(new V1DataList("Empty list", DateTime.Now, 0));
+            collection.Add(new V1DataArray("Uniform array", DateTime.Now, 2, 2, 1.5, 1.5, Field.UniformField));
+
+            V1DataList extreme_list = new("List with extreme value", DateTime.Now);
+            extreme_list.Add(new DataItem(10, 20, new Complex(100, 100)));
+            extreme_list.Add(new DataItem(42, -10, new Complex(0, 0)));
+
+            V1DataList x_list = new("List with convenient X's", DateTime.Now);
+            x_list.Add(new DataItem(1.5, 1000, new Complex(0, 0)));
+            x_list.Add(new DataItem(42, 12, new Complex(0, 0)));
+
+            collection.Add(extreme_list);
+            collection.Add(x_list);
+
+            Console.WriteLine("---------- MainCollection: ----------");
+            Console.WriteLine(collection.ToLongString("F3"));
+
+
+            Console.WriteLine("---------- Method calls for full collection: ----------");
+            Console.WriteLine($"AverageMagnitude: {collection.AverageMagnitude}");
+            Console.WriteLine($"MostDeviated: {collection.MostDeviated}");
+            Console.WriteLine($"CommonXCoords:");
+            iterator = collection.CommonXCoords;
+            if (iterator != null)
+            {
+                foreach (var item in iterator)
+                {
+                    Console.WriteLine($"  {item}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"no elements, got null");
+            }
         }
     }
 }
